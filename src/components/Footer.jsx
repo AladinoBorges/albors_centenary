b0589a73b504176170791b1287e94926d9b100e5
Aladino Borges <eualadino@gmail.com>
2021-07-24 23:20:53 -0300

    Protótipo: Rodapé.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+function Footer() {
+
+  return (
+    <footer className="footer ">
+      <div className="content has-text-left is-size-6 is-size-7-mobile footer-shadows">
+        <p className="is-uppercase">
+          Fundação Hospital Centenário
+        </p>
+        <p>
+          Avenida Theodomiro Porto da Fonseca, 799, Fião<br/>
+          São Leopoldo, RS<br/>
+          CEP: 93.020-645 | Fone: (51) 3591-1400<br/>
+        </p>
+        <p className="has-text-right">
+          <br/>
+          <br/>
+          Desenvolvido por <a href="https://www.linkedin.com/in/aladinoborges"><strong>ALBORS</strong></a>.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
