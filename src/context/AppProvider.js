92a593ce99fa868269efcaeaceda95be85a570cf
Aladino Borges <eualadino@gmail.com>
2021-07-25 20:39:46 -0300

    Estado Global: lidar com o loading.

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 export default function AppProvider({ children }) {
-  const [globalState, setGlobalState] = useState({});
+  const [globalState, setGlobalState] = useState({
+    loading: true,
+    stopLoader: false
+  });
+
   const context = { globalState, setGlobalState };
 
   return (
