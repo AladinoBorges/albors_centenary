b9ab5d7db34aae08d7744e6626420e9ab72dcb38
Aladino Borges <eualadino@gmail.com>
2021-07-24 23:23:47 -0300

    Protótipo: notícias e editais.

---
+++ src/pages/NewsEditals.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import Tabs from "../components/TabsContent";
+import Footer from "../components/Footer";
+
+function NewsEditals() {
+
+  return (
+    <div>
+      <Header />
+      <Tabs />
+      <Footer />
+    </div>
+  );
+}
+
+export default NewsEditals;
