ec1a1bd8182ac3ccbddefac63a8b9c549e321089
Aladino Borges <eualadino@gmail.com>
2021-07-25 20:41:24 -0300

    Lógica página inicial.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,39 @@
-import Body from '../components/Body';
-import Footer from '../components/Footer';
+import { useContext, useEffect } from 'react';
+
+import AppContext from '../context/AppContext';
+
 import Hero from '../components/Header';
 import Tabs from '../components/TabsContent';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import Loading from '../components/Loading';
 
 function Home() {
+  const { globalState, setGlobalState } = useContext(AppContext);
+  const { loading, stopLoader } = globalState;
+
+  const SECONDS = 800;
+
+  const handleLoading = () => {
+    setTimeout(() => {
+      if (stopLoader === false) {
+        setGlobalState({ stopLoader: true, loading: false });
+      }
+    }, SECONDS);
+  };
+
+  // COMPONENT DID MOUNT?
+  useEffect(() => {
+    handleLoading();
+  }, []);
+
+  if (loading) return (<Loading show={ loading ? 'loading' : 'is-hidden' } />);
+
   return (
     <div>
+      <NavBar />
       <Hero />
-      <Tabs />
-      <Body />
+      <Tabs selectTab={ 'Home' } />
       <Footer />
     </div>
   );
