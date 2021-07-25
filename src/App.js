2e2d9cbc57624799be85afeb1363daff81e11b65
Aladino Borges <eualadino@gmail.com>
2021-07-24 23:19:35 -0300

    App: Configuração das páginas iniciais.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import AppProvider from './context/AppProvider';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import NewsEditals from './pages/NewsEditals';
 
 import 'bulma/css/bulma.min.css';
 import './App.css';
+import './styles/Header.css';
+import './styles/Body.css';
+import './styles/Footer.css';
 
 function App() {
   return (
@@ -13,7 +17,8 @@ function App() {
     <AppProvider>
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route exact path="/about" component={ About } />
+        <Route exact path="/sobre" component={ About } />
+        <Route exact path="/conteudo" component={ NewsEditals } />
       </Switch>
   </AppProvider>
     </div>
